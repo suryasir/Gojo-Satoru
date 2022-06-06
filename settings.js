9d08750996a02d9426a01d6980f7e20b1fd69d45
suryasir <106769996+suryasir@users.noreply.github.com>
2022-06-06 16:33:27 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -19,9 +19,9 @@ global.APIKeys = {
 }
 
 //customize settings\\
-global.owner = ['Your Number']
-global.premium = ['Your Number']
-global.ownernomer = 'Your Number'
+global.owner = ['8967545396']
+global.premium = ['8967545396']
+global.ownernomer = '8967545396'
 global.ownername = '🐦Star Boy Nexus'
 global.botname = 'Gojo-Satoru'
 global.footer = '©NexusNwInc.'
